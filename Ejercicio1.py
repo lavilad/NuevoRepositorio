100c85e4d16c694e24bee9ba7a1c9a67a832be08
DIANA AVILA <dianaavil@gmail.com>
2024-04-06 21:09:14 -0600

    mis primieros cambios

---
+++ Ejercicio1.py
@@ -0,0 +1,12 @@
+import math
+
+def calcular_area_circulo(radio):
+    return math.pi * radio**2
+
+def main():
+    radio = float(input("Ingresa el radio del círculo: "))
+    area = calcular_area_circulo(radio)
+    print("El área del círculo es:", area)
+
+if __name__ == "__main__":
+    main()
